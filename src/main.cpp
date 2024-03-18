a8ddf940cc0125bb672a3a374e88083b8d5aa563
Chara_VerKys <charaverk@gmail.com>
2024-03-18 06:21:25 +0300

    переделал на асинхрон работу тестовую функцию

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,15 @@ class SSHSession : public std::enable_shared_from_this<SSHSession>
     std::string _команда;
     LIBSSH2_SESSION *_session;
     LIBSSH2_CHANNEL *_channel;
+    std::vector<COMMANDS> currentDoCommands;
+    std::string writableCommand;
+    std::stringstream ss;
+    std::stringstream part_of_ss;
+    std::string str;
+    char buffer[4096];
+    int cmd_exit_status;
+    std::regex expect;
+    size_t iteration;
 
 public:
     SSHSession(asio::io_context &io_context,
@@ -28,188 +37,239 @@ public:
                                              _команда(команда)
 
     {
-
-        // std::cout << "инициализирован конструктор для команды " + команда << std::endl;
+        this->iteration = 0;
+        std::cout << "инициализирован конструктор для команды " + _команда << std::endl;
     }
 
     void connect()
     {
         asio::ip::tcp::endpoint endpoint(asio::ip::address::from_string(_hostStringIp), 22);
 
-        std::cout << asio::ip::address::from_string(_hostStringIp) << std::endl;
         auto self = shared_from_this();
-
         _socket.async_connect(endpoint, [this, self](const asio::error_code &ec)
                               {
-                                   int handshake_result = 2147483647;
-                                  if (!ec)
-                                  {
+        if (!ec)
+        {
+            _session = libssh2_session_init();
+            if (_session)
+            {
+        std::cout << asio::ip::address::from_string(_hostStringIp) << std::endl;
+                libssh2_session_set_blocking(_session, 0);
+                // Выполняем handshake асинхронно
+                async_handshake();
+            }
+            else
+            {
+                std::cerr << "Error initializing SSH session" << std::endl;
+            }
+        }
+        else
+        {
+            std::cerr << "Error connecting - " << ec.message() << std::endl;
+        } });
+    }
 
-                                      _session = libssh2_session_init();
-                                      if (_session)
-                                      {
-                                        std::cout<< "\nя открылся "<<_команда<<" \n";
-                                          libssh2_session_set_blocking(_session, 0);
-                                          handshake_result = libssh2_session_handshake(_session, _socket.native_handle());
-                                          // оно будет повторяться пока не другая ошибка или удачный...
-                                        while (handshake_result == LIBSSH2_ERROR_EAGAIN)
-                                            {
-                                                handshake_result = libssh2_session_handshake(_session, _socket.native_handle());
-                                            }
+    void async_handshake()
+    {
+        auto self = shared_from_this();
+        int handshake_result = libssh2_session_handshake(_session, _socket.native_handle());
+        if (handshake_result == 0)
+        {
+            std::cerr << "выполнилось хендшейк для команды " << _команда << std::endl;
 
-                                          if (
-                                               handshake_result == 0)
-                                          {
-                                                std::cout<< "и я выполнился "<<_команда<<" \n";
-                                                    
-                                                 authenticate();
-                                                return;
-                                          }
-                                      }
-                                    // поменять выводы логов для конечной реализации, остальное так же
-                                      std::cerr << "Error initializing SSH session/handshake: "<< std::to_string(handshake_result) << std::endl;
-                                  }else{
-                                         std::cerr << "Error connecting - " << ec.message()<< std::endl;
-                                  } }); // колбек
+            authenticate();
+        }
+        else if (handshake_result == LIBSSH2_ERROR_EAGAIN)
+        {
+            // Ждем асинхронно, пока handshake завершится
+            _socket.async_wait(asio::ip::tcp::socket::wait_read, [this, self](const asio::error_code &ec)
+                               {
+            std::cerr << "запустился асинх авейт хендшейк для "<< _команда << std::endl;
+                                
+            if (!ec)
+            {
+                async_handshake();
+            }
+            else
+            {
+                std::cerr << "Error during handshake - " << ec.message() << std::endl;
+            } });
+        }
+        else
+        {
+            std::cerr << "Error during handshake - " << handshake_result << std::endl;
+        }
     }
 
     void authenticate()
     {
 
-        while (true)
+        auto self = shared_from_this();
+        int аутен_result = libssh2_userauth_password(_session, _username.c_str(), _password.c_str());
+        if (аутен_result == 0)
         {
-            // Обработка событий (может быть использован select(), epoll() или другие механизмы)
-
-            // Попытка выполнения операции (например, рукопожатие или аутентификация)
-            int аутен_result = libssh2_userauth_password(_session, _username.c_str(), _password.c_str());
-
-            // Проверка результата операции
-            if (аутен_result == 0)
-            {
-                // Операция завершилась успешно
-                std::cout << "и я аутентифицировался " << _команда << " \n";
-                break; // Выход из цикла, если не нужно ничего еще делать
-            }
-            else if (аутен_result == LIBSSH2_ERROR_EAGAIN)
-            {
-                // Операция не завершилась немедленно, продолжаем ожидать
-                continue;
-            }
-            else
-            {
-                // Обработка других ошибок
-                // Например, если аутентификация не удалась, выходим из цикла
-                std::cerr << "Error: " << аутен_result << std::endl;
-                break;
-            }
+            std::cout << "и я аутентифицировался " << _команда << " \n";
+            currentCommands_vector_init();
         }
-        executeCommands();
+        else if (аутен_result == LIBSSH2_ERROR_EAGAIN)
+        {
+            // Ждем асинхронно, пока handshake завершится
+            _socket.async_wait(asio::ip::tcp::socket::wait_read, [this, self](const asio::error_code &ec)
+                               {
+                                   std::cout << "и я зашёл в тело асинх аутент " << _команда << " \n"; // для аутен ошибка неверный логин пароль -18
+
+                                   if (!ec)
+                                   {
+                                       authenticate();
+                                   }
+                                   else
+                                   {
+                                       std::cerr << "Error during userauth - " << ec.message() << std::endl;
+                                   } });
+        }
+        else
+        {
+            std::cerr << "Error during userauth - " << аутен_result << std::endl;
+        }
     }
 
-    void executeCommands()
+    void currentCommands_vector_init()
     {
-
-        std::vector<COMMANDS> currentDoCommands = {
+        currentDoCommands = {
             COMMANDS(_команда, "", 0),
             COMMANDS("echo expect_string", ".*ct.st.*", 0),
             COMMANDS("sleep 5", "", 0)};
+        one_iteration(); // всё ещё тело аутен калбека кста
+    }
 
-        std::string writableCommand;
-        std::stringstream ss;
-        std::string str;
-        char buffer[4096];
-        int rc;
-int cmd_exit_status;
- std::regex expect;
+    void one_iteration()
+    {
 
-        for (COMMANDS &command : currentDoCommands)
+        if (!(iteration < currentDoCommands.size())) // всё ещё калбек аутентифика, то есть если команд 0 то не выполнит ничего
         {
-            writableCommand = "\n\nотправленна команда\t " + command.cmd + " \n\tРезультат:\n";
+            str += ss.str();
+            std::cout << "\tи я полученная строка лога" << str << " \n";
+            return; // логика завершения, по идее должен вызвать деструктор прям ща
+        }
+
+        auto self = shared_from_this();
+        _channel = libssh2_channel_open_session(_session);
+
+        if (_channel)
+        {
+            writableCommand = "\n\nотправленна команда\t " + currentDoCommands[iteration].cmd + " \n\tРезультат:\n";
             ss << writableCommand;
+            std::cout << "канал открылся (вызов) для" << _команда << std::endl;
+            // выполняю команды
+            execute_one_command();
+        }
+        else if (libssh2_session_last_errno(_session) == LIBSSH2_ERROR_EAGAIN)
+        {
 
-            // открываю канал для одной блять команды
-            while (true)
+            _socket.async_wait(asio::ip::tcp::socket::wait_read, [this, self](const asio::error_code &ec)
+                               {
+                                   std::cout << "и я зашёл в тело асинх старт ченел (вон этерейшн) " << _команда << " \n"; 
+
+                                   if (!ec)
+                                   {
+                                       one_iteration();
+                                   }
+                                   else
+                                   {
+                                       std::cerr << "Error during channal create - " << ec.message() << std::endl;
+                                   } });
+        }
+        else
+        {
+            std::cerr << "Failed to open channel with err_code " << libssh2_session_last_errno(_session) << std::endl;
+        }
+    }
+
+    void execute_one_command()
+    {
+        libssh2_channel_exec(_channel, currentDoCommands[iteration].cmd.c_str());
+        std::cout << "отправленна команда " + currentDoCommands[iteration].cmd + " для " + _hostStringIp + "\n";
+        read_one_command();
+    }
+
+    void read_one_command()
+    {
+
+        int rc = libssh2_channel_read(_channel, buffer, sizeof(buffer));
+        auto self = shared_from_this();
+        if (rc > 0)
+        {
+            std::cout << "считало в read_one_command для " << _команда << " \n";
+            part_of_ss.write(buffer, rc);
+            read_one_command();
+        }
+        else if (rc < 0 && rc != LIBSSH2_ERROR_EAGAIN)
+        {
+            str += " не удалось считать вывод от устройства с кодом ошибки " + std::to_string(rc) + " \n" + ss.str() + part_of_ss.str();
+            std::cerr << "Error: " << str << std::endl;
+        }
+        else if (rc == 0)
+        {
+
+            end_one_command(); // продолжить выполнение (этот же калбек просто вынес в другую функцию)
+        }
+        else if (rc == LIBSSH2_ERROR_EAGAIN) // ошибка говорящая что не все байты получены
+        {
+
+            _socket.async_wait(asio::ip::tcp::socket::wait_read, [this, self](const asio::error_code &ec)
+                               {
+                                   std::cout << "и я зашёл в тело асинх read_one_command для " << _команда << " \n"; 
+
+                                   if (!ec)
+                                   {
+
+                                       read_one_command();
+                                   }
+                                   else
+                                   {
+                                       std::cerr << "Error during read answer - " << ec.message() << std::endl;
+                                   } });
+        }
+    }
+
+    void end_one_command()
+    {
+
+        cmd_exit_status = libssh2_channel_get_exit_status(_channel);
+        if (!(cmd_exit_status == currentDoCommands[iteration].code))
+        {
+            str += "неверный код результата для команды" + currentDoCommands[iteration].cmd + ".Ожидалось" + std::to_string(currentDoCommands[iteration].code) + ", а в ответе" + std::to_string(cmd_exit_status);
+            // влог - это же + подробнее для этого хоста можно посмотреть в show errHosts
+             str += "\n\n" + ss.str() + part_of_ss.str();
+            std::cerr << "Error: " << str << std::endl;
+            // записать хост в таблицу errHosts throw неверный код результата для команды
+        }
+
+        // проверить ответ на равенство ожидаемому
+        if (currentDoCommands[iteration].expect != "")
+        {
+            std::string str2 = part_of_ss.str();
+            expect = std::regex(currentDoCommands[iteration].expect); // содержет строку в которой описана регулярка
+            if (!(std::regex_search(part_of_ss.str(), expect)))
             {
-                _channel = libssh2_channel_open_session(_session);
-
-                if (_channel)
-                {
-                    // Канал успешно открыт, выходим из цикла
-                    std::cout << "канал открылся" << std::endl;
-                    // выполняю команды
-                    break;
-                }
-                else if (libssh2_session_last_errno(_session) == LIBSSH2_ERROR_EAGAIN)
-                {
-                    // без ожидания, сейчас проверяю работоспособность
-                    continue;
-                }
-                else
-                {
-                    std::cerr << "Failed to open channel with err_code " << libssh2_session_last_errno(_session) << std::endl;
-                    break; // Выход из цикла в случае ошибки
-                }
-            }
-            // открыл
-
-            libssh2_channel_exec(_channel, command.cmd.c_str());
-            std::cout << "отправленна команда " + command.cmd + " для " + _hostStringIp + "\n";
-
-            do
-            {
-                rc = libssh2_channel_read(_channel, buffer, sizeof(buffer));
-                if (rc > 0)
-                {
-                    ss.write(buffer, rc);
-                }
-                else if (rc == LIBSSH2_ERROR_EAGAIN) // ошибка говорящая что не все байты получены
-                {
-                    continue;
-                }
-                else if (rc < 0)
-                {
-                    str += " не удалось считать вывод от устройства с кодом ошибки "+std::to_string(rc)+" \n" + ss.str();
-                    std::cerr << "Error: " << str << std::endl;
-                    break;
-                }
-                else // if (rc == 0)
-                {
-                    break;
-                }
-            } while (true);
-
-            cmd_exit_status = libssh2_channel_get_exit_status(_channel);
-            if (!(cmd_exit_status == command.code))
-            {
-                str += "неверный код результата для команды" + command.cmd +".Ожидалось" + std::to_string(command.code) +", а в ответе" + std::to_string(cmd_exit_status);
-                //влог - это же + подробнее для этого хоста можно посмотреть в show errHosts str += \n\n + ss.str();
+                // если не (регулярки совпадают) то выкинуть ошибку
+                
+                   str += "неожиданный output команды " + currentDoCommands[iteration].cmd +" для "+ _hostStringIp +". Ожидалось \n\n" + currentDoCommands[iteration].expect +"\n\n а в ответе\n\n" + part_of_ss.str() ;
+                   //host.log = str;
+             str += "\n\n" + ss.str() + part_of_ss.str();
                 std::cerr << "Error: " << str << std::endl;
-               // записать хост в таблицу errHosts throw неверный код результата для команды
+                //   влог - неожиданный output команды + command.cmd + для + hostIP +.подробнее для этого хоста можно посмотреть в show errHosts записать хост в таблицу errHosts throw неожиданный output команды
             }
+        }
 
-
-
-            //проверить ответ на равенство ожидаемому
-                if (command.expect != "")
-            {
-                expect = std::regex(command.expect); //содержет строку в которой описана регулярка
-                if (!(std::regex_match(ss.str(), expect)))
-                { 
-                    // если не (регулярки совпадают) то выкинуть ошибку
-                                     
-                 //   str += неожиданный output команды + command.cmd + для + hostIP +.Ожидалось \n\n + command.expect +\n\n а в ответе\n\n + ss.str() host.log = str;
-                 //   влог - неожиданный output команды + command.cmd + для + hostIP +.подробнее для этого хоста можно посмотреть в show errHosts записать хост в таблицу errHosts throw неожиданный output команды
-                }
-            }
-
-            libssh2_channel_close(_channel);
-            libssh2_channel_free(_channel);
-            _channel = nullptr;
-
-        } // for(COMMANDS &command : currentDoCommands)
-
-        str += ss.str();
-        std::cout << "\tи я полученная строка лога" << str << " \n";
+           ss << part_of_ss.str(); 
+           part_of_ss.str("");
+        libssh2_channel_close(_channel);
+        libssh2_channel_free(_channel);
+        _channel = nullptr;
+        ++iteration;
+                              
+            one_iteration(); // ахуеть а теперь из калбека рид команд вызываю
     }
 
     ~SSHSession()
@@ -230,6 +290,7 @@ int cmd_exit_status;
         }
     }
 };
+
 int main()
 {
 
@@ -238,7 +299,7 @@ int main()
     try
     {
         asio::io_context io;
-        for (int i = 0; i < 2; ++i)
+        for (int i = 0; i < 9; ++i)
         {
             std::string str = "touch testssh-" + std::to_string(i);
             com[i] = new char[str.length() + 1];
@@ -247,7 +308,7 @@ int main()
             session->connect();
         }
 
-        for (int i = 9; i < 11; ++i)
+        for (int i = 9; i < 19; ++i)
         {
             std::string str = "touch testssh-" + std::to_string(i);
             com[i] = new char[str.length() + 1];
