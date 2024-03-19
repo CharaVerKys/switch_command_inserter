a04ee163803ae477cb40ecc49edeb3e58ba7f497
Chara_VerKys <charaverk@gmail.com>
2024-03-19 20:41:48 +0300

    переделал SSHSession на оболочку

--- include/main.hpp
+++ include/main.hpp
@@ -4,12 +4,11 @@
 #include <asio.hpp>
 #include <CWRconfigs.hpp>
 #define TableNameForSSH "activeHostsSSH"
+
 #include <database.hpp>
 #include <headerlibSwitchCase.hpp>
 #include <CommandsCommiterSSH.hpp>
 // #include <logging.hpp> // уже добавил в CWRconfigs.hpp, чисто на всякий случай закоментил
-
-
 
 // класс тестовый
 //
@@ -20,7 +19,6 @@
 #include <libssh2.h>
 class SSHSession : public std::enable_shared_from_this<SSHSession>
 {
-
     asio::io_context &_io_context;
     asio::ip::tcp::socket _socket;
     std::string _hostStringIp;
@@ -34,11 +32,15 @@ class SSHSession : public std::enable_shared_from_this<SSHSession>
     std::stringstream ss;
     std::stringstream part_of_ss;
     std::string str;
+    const char *currentCstrCommand;
     char buffer[4096];
     int cmd_exit_status;
     std::regex expect;
     std::regex model_regex;
+    std::regex end_of_read = std::regex("\\S+[#$>]$", std::regex::ECMAScript);
     size_t iteration;
+    bool is_end_of_readq;
+    bool one_again_taked;
 
 public:
     void currentCommands_vector_init()
@@ -59,7 +61,7 @@ public:
           // то есть на каждый существующий конфиг я для каждого существующего хоста (в списке валидных) перебираю пока не найдётся, начиная с первого
           // то есть, если первая регулярка .* то все хосты будут выполняться к первой.
 
-        one_iteration(); // всё ещё тело аутен калбека кста
+        init_chanel(); // всё ещё тело аутен калбека кста
     }
 
     SSHSession(asio::io_context &io_context,
@@ -171,37 +173,27 @@ public:
         }
     }
 
-    void one_iteration()
+    void init_chanel()
     {
-
-        if (!(iteration < currentDoCommands.size())) // всё ещё калбек аутентифика, то есть если команд 0 то не выполнит ничего
-        {
-            str += ss.str();
-            std::cout << "\tи я полученная строка лога" << str << " \n";
-            return; // логика завершения, по идее должен вызвать деструктор прям ща
-        }
-
+        // всё ещё тело аутен калбека кста
         auto self = shared_from_this();
         _channel = libssh2_channel_open_session(_session);
 
         if (_channel)
         {
-            writableCommand = "\n\nотправленна команда\t " + currentDoCommands[iteration].cmd + " \n\tРезультат:\n";
-            ss << writableCommand;
-            std::cout << "канал открылся (вызов) для" << _команда << std::endl;
-            // выполняю команды
-            execute_one_command();
+            std::cout << "канал открылся для " << _команда << std::endl;
+            init_shell();
         }
         else if (libssh2_session_last_errno(_session) == LIBSSH2_ERROR_EAGAIN)
         {
 
             _socket.async_wait(asio::ip::tcp::socket::wait_read, [this, self](const asio::error_code &ec)
                                {
-                                   std::cout << "и я зашёл в тело асинх старт ченел (вон этерейшн) " << _команда << " \n"; 
+                                   std::cout << "и я зашёл в тело асинх старт ченел " << _команда << " \n"; 
 
                                    if (!ec)
                                    {
-                                       one_iteration();
+                                       init_chanel();
                                    }
                                    else
                                    {
@@ -214,22 +206,176 @@ public:
         }
     }
 
-    void execute_one_command()
+    void init_shell()
     {
-        libssh2_channel_exec(_channel, currentDoCommands[iteration].cmd.c_str());
+
+        auto self = shared_from_this();
+        int rc = libssh2_channel_shell(_channel);
+        if (rc)
+        {
+            std::cout << "канал шел открылся для " << _команда << std::endl;
+
+            read_a_fuck(); // логика для запуска большого количества команд асинхронно (цикл while для асинхронной реализации в event loop)
+        }
+        else if (rc == LIBSSH2_ERROR_EAGAIN)
+        {
+
+            _socket.async_wait(asio::ip::tcp::socket::wait_read, [this, self](const asio::error_code &ec)
+                               {
+                                   std::cout << "и я зашёл в тело асинх старт shell " << _команда << " \n"; 
+
+                                   if (!ec)
+                                   {
+                                       init_shell();
+                                   }
+                                   else
+                                   {
+                                       std::cerr << "Error during shell create - " << ec.message() << std::endl;
+                                   } });
+        }
+        else
+        {
+            std::cerr << "Failed to open shell with err_code " << rc << std::endl;
+        }
+    }
+
+    void read_a_fuck()
+    {
+
+        std::regex read_a_fucking_label = std::regex("#"); // объявлено выше в остальных переменных
+
+        int rc = libssh2_channel_read(_channel, buffer, sizeof(buffer));
+
+        if (rc > 0)
+        {
+            std::cout << "считало в read_a_fuck для " << _команда << " \n";
+            part_of_ss.write(buffer, rc); // здесь находится лейбл, сейчас не используется и отчищается перед началом цикла
+            read_a_fuck();
+        }
+        else if (rc < 0 && rc != LIBSSH2_ERROR_EAGAIN)
+        {
+            str += " не удалось считать вывод от устройства с кодом ошибки " + std::to_string(rc) + " в read_a_fuck \n" + ss.str() + part_of_ss.str();
+            std::cerr << "Error: " << str << std::endl;
+        }
+
+        else if (std::regex_search(part_of_ss.str(), read_a_fucking_label)) // главное чтобы проверка была до открытия сокета
+        {
+            part_of_ss.str(""); //  отчищается перед началом цикла
+            one_iteration();
+        }
+        else if (rc == LIBSSH2_ERROR_EAGAIN) // ошибка говорящая что не все байты получены
+        {
+            auto self = shared_from_this();
+            _socket.async_wait(asio::ip::tcp::socket::wait_read, [this, self](const asio::error_code &ec)
+                               {
+                                   std::cout << "и я зашёл в тело асинх read_a_fuck для " << _команда << " \n";
+
+                                   if (!ec)
+                                   {
+                                       read_a_fuck();
+                                   }
+                                   else
+                                   {
+                                       std::cerr << "Error during read_a_fuck answer - " << ec.message() << std::endl;
+                                   } });
+        }
+    }
+
+    void one_iteration()
+    {
+
+        if (!(iteration < currentDoCommands.size())) // если команд 0 то не выполнит ничего
+        {
+            str += ss.str();
+            std::cout << "\tи я полученная строка лога" << str << " \n";
+            return; // логика завершения, по идее должен вызвать деструктор прям ща
+        }
+        else
+        {
+            // выполняю командe
+            writableCommand = "\n\nотправленна команда\t " + currentDoCommands[iteration].cmd + " \n\tРезультат:\n";
+            ss << writableCommand;
+            execute_one_command();
+        }
+    }
+
+    void execute_one_command() // вообще сделал что-то типо pipeline но хз правильно ли, зато понятно, не так востребовано как с предыдущими но да и ладно
+    {
+
+        currentCstrCommand = (currentDoCommands[iteration].cmd += "\n").c_str();
+        libssh2_channel_write(_channel, currentCstrCommand, strlen(currentCstrCommand));
         std::cout << "отправленна команда " + currentDoCommands[iteration].cmd + " для " + _hostStringIp + "\n";
+        is_end_of_readq = false;
+        one_again_taked = true; // чтобы выполнелась отправка первого энтера
         read_one_command();
     }
+
+    void check_end_of_read(uint16_t buffer_point_add)
+    { // обработчик проверки получения всех отправленных данных
+
+        if (std::regex_search(buffer, buffer + buffer_point_add, end_of_read))
+        {
+            is_end_of_readq = true;
+            return;
+        }
+        else
+        {
+        }
+
+        // else contunue
+
+        if (one_again_taked)
+        {
+            libssh2_channel_write(_channel, "\n", strlen("\n"));
+        } // отправляется только если было прочитано до этого (или при старте)
+        int rc = libssh2_channel_read(_channel, buffer, sizeof(buffer));
+
+        if (rc > 0)
+        {
+            std::cout << "считало в check_end_of_read для " << _команда << " \n";
+            part_of_ss.write(buffer, rc);
+            one_again_taked = true;
+            check_end_of_read(rc);
+        }
+        else if (rc < 0 && rc != LIBSSH2_ERROR_EAGAIN)
+        {
+            str += " не удалось считать вывод от устройства с кодом ошибки " + std::to_string(rc) + " \n" + ss.str() + part_of_ss.str();
+            std::cerr << "Error: " << str << std::endl;
+        }
+        else if (rc == LIBSSH2_ERROR_EAGAIN) // ошибка говорящая что не все байты получены
+        {
+            one_again_taked = false; // это цикличное ожидание, не нужно отправлять нужный энтер
+            auto self = shared_from_this();
+            _socket.async_wait(asio::ip::tcp::socket::wait_read, [this, self, rc](const asio::error_code &ec)
+                               {
+                                   std::cout << "и я зашёл в тело асинх check_end_of_read для " << _команда << " \n"; 
+
+                                   if (!ec)
+                                   {
+                                       check_end_of_read(rc);
+                                   }
+                                   else
+                                   {
+                                       std::cerr << "Error during check_end_of_read answer - " << ec.message() << std::endl;
+                                   } });
+        }
+        else
+        {
+        } // это произойдёт только в том случае если отправится eof
+    }
+
+    // РИД НУЖНО ОБЕРНУТЬ В ТАЙМЕР НА 15 МИНУТ В ИТОГОВОЙ РЕАЛИЗАЦИИ (от экзек до енд оф ком)
 
     void read_one_command()
     {
 
         int rc = libssh2_channel_read(_channel, buffer, sizeof(buffer));
-        auto self = shared_from_this();
+
         if (rc > 0)
         {
             std::cout << "считало в read_one_command для " << _команда << " \n";
             part_of_ss.write(buffer, rc);
+            check_end_of_read(rc);
             read_one_command();
         }
         else if (rc < 0 && rc != LIBSSH2_ERROR_EAGAIN)
@@ -237,13 +383,14 @@ public:
             str += " не удалось считать вывод от устройства с кодом ошибки " + std::to_string(rc) + " \n" + ss.str() + part_of_ss.str();
             std::cerr << "Error: " << str << std::endl;
         }
-        else if (rc == 0)
-        {
 
+        else if (is_end_of_readq) // главное чтобы проверка была до открытия сокета
+        {
             end_one_command(); // продолжить выполнение (этот же калбек просто вынес в другую функцию)
         }
         else if (rc == LIBSSH2_ERROR_EAGAIN) // ошибка говорящая что не все байты получены
         {
+            auto self = shared_from_this();
 
             _socket.async_wait(asio::ip::tcp::socket::wait_read, [this, self](const asio::error_code &ec)
                                {
@@ -251,7 +398,6 @@ public:
 
                                    if (!ec)
                                    {
-
                                        read_one_command();
                                    }
                                    else
@@ -292,12 +438,10 @@ public:
 
         ss << part_of_ss.str();
         part_of_ss.str("");
-        libssh2_channel_close(_channel);
-        libssh2_channel_free(_channel);
-        _channel = nullptr;
+
         ++iteration;
 
-        one_iteration(); // ахуеть а теперь из калбека рид команд вызываю
+        one_iteration(); // продолжаю пока в массиве команд ещё есть данные
     }
 
     ~SSHSession()
@@ -325,24 +469,13 @@ public:
 //
 //
 
-
 // initVars.cpp
-void initVars(std::filesystem::path executable_path, const int & argc, char const* argv[]);
-
+void initVars(std::filesystem::path executable_path, const int &argc, char const *argv[]);
 
 // rootCallFunctions.cpp
 void rootScan(int argc, char const *argv[]);
 
-
-
-
-
-
-
-
-
-
-// scanNetwork.cpp 
+// scanNetwork.cpp
 struct SNparsedNetworkHost
 {
     uint32_t network;
@@ -358,16 +491,11 @@ ActiveHOSTS ScanNetwork(std::vector<HOST> &hosts);
 
 std::vector<HOST> SNinitHostsVector(SNparsedNetworkHost &IpPool);
 void SNcheck_port_async(HOST &host, asio::io_context &io_context, HOST::PORT &port, std::vector<HOST> &validHosts);
-void SNcheck_port_async(HOST &host, asio::io_context &io_context, HOST::PORT &port, std::vector<HOST> &validHosts,  std::vector<HOST> &refusedHosts);
+void SNcheck_port_async(HOST &host, asio::io_context &io_context, HOST::PORT &port, std::vector<HOST> &validHosts, std::vector<HOST> &refusedHosts);
 
-
-//мусорная функция, не используется
+// мусорная функция, не используется
 bool is_port_open(const uint32_t ip_address, int port);
-
 
 // parseIpMaskToIpPool.cpp
 SNparsedNetworkHost parseIpMaskToIpPool(std::string ipAndMask);
 uint32_t ipToBin(const std::string &ip);
-
-
-
